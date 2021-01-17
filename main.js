7a1ab1f791989f7ee8a133933362febbf3b28bd3
Maximilian Fischer <maximilian.fischer@route242.com>
2021-01-17 12:17:00 +0100

    Check for undefined

--- main.js
+++ main.js
@@ -2029,22 +2029,23 @@ function createNotificationStates(serialOrName) {
                 }
             }
         }
-        adapter.log.info(timers);
-        /*
-        timers.forEach(element => {
-            createState(devId + 'Timers.' + element.id +'.nextTimerDate', function () {
+        if(timers !== undefined)
+        {
+            timers.forEach(element => {
+                createState(devId + 'Timers.' + element.id +'.nextTimerDate', function () {
+                });
+    
+                if(noti.status == 'ON')
+                {
+                    setOrUpdateObject(devId + 'Timers.' + element.id +'.nextTimerDate', {common: {type: 'number', role: 'date', name: 'Unix epoch timestamp for next timer'}}, nextTimerObject ? (Date.now() + nextTimerObject.remainingTime) : 0, nextTimerObject ? nextTimerObject.set : null);
+                }
+                else
+                {
+                    deleteObject(devId + 'Timers.' + element.id);
+                }
+              
             });
-
-            if(noti.status == 'ON')
-            {
-                setOrUpdateObject(devId + 'Timers.' + element.id +'.nextTimerDate', {common: {type: 'number', role: 'date', name: 'Unix epoch timestamp for next timer'}}, nextTimerObject ? (Date.now() + nextTimerObject.remainingTime) : 0, nextTimerObject ? nextTimerObject.set : null);
-            }
-            else
-            {
-                deleteObject(devId + 'Timers.' + element.id);
-            }
-          
-        });*/
+        }      
         
         setOrUpdateObject(devId + '.Timer.nextTimerDate', {common: {type: 'number', role: 'date', name: 'Unix epoch timestamp for next timer'}}, nextTimerObject ? (Date.now() + nextTimerObject.remainingTime) : 0, nextTimerObject ? nextTimerObject.set : null);
     }
